4fe2ad578dae31d98ae4e69ba8608b73de655bc7
lucas-fernandes-data <76710848+lucas-fernandes-data@users.noreply.github.com>
2021-09-09 16:34:44 -0300

    ajust base stories and gtag

--- src/components/layouts/Base/stories.tsx
+++ src/components/layouts/Base/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react'
-import Base from '.'
+import Base, { BaseProps } from '.'
 
 export default {
   title: 'Base',
   component: Base
 } as Meta
 
-export const Default: Story = () => <Base />
+export const Default: Story<BaseProps> = (args) => <Base {...args} />
