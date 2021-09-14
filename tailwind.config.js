c4a9011bd950659e70e73c3e9efb61d006d1adeb
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2021-09-14 10:50:43 -0300

    removed unnecessary fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    fontSize: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-    },
     extend: {
       colors: {
         gray: {
@@ -26,7 +14,7 @@ module.exports = {
           600: '#718096',
           700: '#4a5568',
           800: '#2d3748',
-          900: '#1a202c',
+          900: '#1a202c'
         },
         blue: {
           100: '#ebf8ff',
@@ -37,11 +25,11 @@ module.exports = {
           600: '#3182ce',
           700: '#2b6cb0',
           800: '#2c5282',
-          900: '#2a4365',
-        },
-      },
-    },
+          900: '#2a4365'
+        }
+      }
+    }
   },
   variants: {},
-  plugins: [],
-};
+  plugins: []
+}
